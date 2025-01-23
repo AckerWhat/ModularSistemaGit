3a0b586be502a24556716026fa6cdcc4a8187a88
AckerWhat <nicolaamoruso1015@gmail.com>
2025-01-23 13:23:14 -0400

    Delete archivo.py

--- archivo.py
+++
@@ -1,7 +0,0 @@
-class Archivo:
-    def __init__(self, nombre, contenido=""):
-        self.nombre = nombre
-        self.contenido = contenido
-
-    def __str__(self):
-        return f"{self.nombre}: {self.contenido}"
