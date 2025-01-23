4508ddd285fe5b4503e7898ab8fdb4480bdf5f87
AckerWhat <nicolaamoruso1015@gmail.com>
2025-01-23 13:23:34 -0400

    Delete rama.py

--- rama.py
+++
@@ -1,7 +0,0 @@
-class Rama:
-    def __init__(self, nombre, commit_inicial=None):
-        self.nombre = nombre
-        self.commit_reciente = commit_inicial
-
-    def __str__(self):
-        return f"Rama {self.nombre}, último commit: {self.commit_reciente}"
